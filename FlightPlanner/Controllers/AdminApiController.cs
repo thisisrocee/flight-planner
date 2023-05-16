9e4ec91dd220350f4d05e2965a400a561d47b725
ThisIsRocee <roberts.juniors@gmail.com>
2023-05-16 14:33:58 +0300

    Update 1.0

---
+++ FlightPlanner/Controllers/AdminApiController.cs
@@ -0,0 +1,49 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("admin-api")]
+    [ApiController]
+    [Authorize]
+    public class AdminApiController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult GetFlights(int id)
+        {
+            var flight = FlightStorage.GetFlight(id);
+
+            return flight == null ? NotFound() : Ok(flight);
+        }
+
+        [HttpPut]
+        [Route("flights")]
+        public IActionResult AddFlight(Flight flight)
+        {
+            if (FlightStorage.IsInvalidFlight(flight))
+            {
+                return BadRequest();
+            }
+
+            if (FlightStorage.FlightExists(flight))
+            {
+                return Conflict();
+            }
+
+            return Created("", FlightStorage.AddFlight(flight));
+        }
+
+        [HttpDelete]
+        [Route("flights/{id}")]
+        public IActionResult DeleteFlight(int id)
+        {
+            FlightStorage.DeleteFlight(id);
+
+            return Ok();
+        }
+    }
+}
---
+++ FlightPlanner/Controllers/CustomerApiController.cs
@@ -0,0 +1,43 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CustomerApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("airports")]
+        public IActionResult GetAirport(string search)
+        {
+            var result = FlightStorage.GetAirport(search);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("flights/search")]
+        public IActionResult SearchFlight(SearchFlightRequest searchFlight)
+        {
+            if (FlightStorage.IsInvalidFlightSearch(searchFlight))
+            {
+                return BadRequest();
+            }
+
+            var result = FlightStorage.SearchFlight(searchFlight);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult GetFlight(int id)
+        {
+            var flight = FlightStorage.GetFlight(id);
+
+            return flight == null ? NotFound() : Ok(flight);
+        }
+    }
+}
