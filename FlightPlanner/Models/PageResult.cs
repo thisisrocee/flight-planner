3056015eed09432cee5987c51c70946614de7f14
ThisIsRocee <roberts.juniors@gmail.com>
2023-05-16 14:23:48 +0300

    Update 1st iteration

---
+++ FlightPlanner/Models/PageResult.cs
@@ -0,0 +1,9 @@
+﻿namespace FlightPlanner.Models
+{
+    public class PageResult
+    {
+        public int Page { get; set; }
+        public int TotalItems { get; set; }
+        public List<Flight> Items { get; set; }
+    }
+}
